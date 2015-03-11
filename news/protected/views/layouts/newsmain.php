67886a149584bde136744cddfb03ae65758db6b1
wangxingchao <sosowangxingchao@126.com>
2015-03-11 13:20:57 +0800

    新闻集合页面底部链接修改

--- news/protected/views/layouts/newsmain.php
+++ news/protected/views/layouts/newsmain.php
@@ -19,7 +19,7 @@
 
   <div class="expo_nav" style="width:940px;margin:0 auto;line-height:26px;height:36px;">
    <span><a href="http://www.renshikaoshi.net" target="_blank"><b>网站首页</b></a></span><a href="http://www.renshikaoshi.net/En/"><font color="red">职称英语</font></a>-<a href="http://www.renshikaoshi.net/En/lgl/">理工类</a>-<a href="http://www.renshikaoshi.net/En/wsl/">卫生类</a>-<a href="http://www.renshikaoshi.net/jsj/">职称计算机</a>-
-  <a href="http://www.renshikaoshi.net/zyzg/">职业资格</a>-<a href="http://www.renshikaoshi.net/zyzg/ckl/">金融财会</a>-<a href="http://www.renshikaoshi.net/zyzg/jz/">建筑工程</a>-<a href="http://www.renshikaoshi.net/zyzg/yy/">医药卫生</a>-<a href="#">帮助中心</a></div>
+  <a href="http://www.renshikaoshi.net/zyzg/">职业资格</a>-<a href="http://www.renshikaoshi.net/zyzg/ckl/">金融财会</a>-<a href="http://www.renshikaoshi.net/zyzg/jz/">建筑工程</a>-<a href="http://www.renshikaoshi.net/zyzg/yy/">医药卫生</a></div>
   
   
 </div>
@@ -51,10 +51,9 @@
       </div>
 
 <div id="foot" class="area area"  style=" border-top:1px solid #E1E1E1; line-height:22px;padding-top:10px; height:150px;" >
-      <p align="center"><a onClick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://http://www.renshikaoshi.net');return" href="javascript:void(0)" false;="">设置首页</a> - <a href="http://www.renshikaoshi.net/list-2-1.html" rel="nofollow" target="_blank">关于博大</a> - <a href="http://www.cdbroad.com/cdbroad/gysy/18" rel="nofollow" target="_blank">博大公益</a> - <a href="http://wap.cdbroad.com/" rel="nofollow" target="_blank">手机版</a> - <a href="http://www.renshikaoshi.net/list-5-1.html" rel="nofollow" target="_blank">合作联系</a> - <a href="http://www.renshikaoshi.net/list-4-1.html" rel="nofollow" target="_blank">网站声明</a>- <a href="http://www.renshikaoshi.net/old/" target="_blank">旧版</a> - <a href="http://www.renshikaoshi.net/list-445-1.html" rel="nofollow" target="_blank">帮助中心</a> - <a href="http://www.renshikaoshi.net/list-446-1.html" rel="nofollow" target="_blank">友情链接</a> - <a href="#" target="_blank">网站导航</a> </p>
-            <p align="center">Copyright &#169; 2011-2013 Inc. all Rights Reserved. 博大教育 <a href="http://www.renshikaoshi.net/list-4-1.html" target="_blank">版权所有</a> </p>
-       <p align="center"><a href="http://www.renshikaoshi.net" target="_blank">成都博大教育科技有限公司</a>&nbsp;&nbsp;蜀ICP备07505764号&nbsp;&nbsp; 
- </p></div>
+      <p align="center"><a onClick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://http://www.renshikaoshi.net');return" href="javascript:void(0)" false;="">设置首页</a> - <a href="http://g.renshikaoshi.net/" rel="nofollow" target="_blank">手机版</a> - <a href="http://www.renshikaoshi.net/sitemaps.html" target="_blank">网站导航</a> </p>
+      <p align="center">Copyright©2012-2018 RENSHIKAOSHI, All Rights Reserved. 备案编号: 皖ICP备13019968号-1 </p>
+</div>
 </body>
 
 </html>
